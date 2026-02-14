3b75c175bdf5c8962270fe368f75a59ea5c8df3e
Vishwas Chaurasia <www.vishwaschaurasia@gmail.com>
2026-02-14 20:15:59 +0530

    commit 2 day 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import { RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react'
+import AppShell from './components/AppShell'
+// import CreateInvoice from './pages/CreateInvoice'
+// import InvoicesPage from './pages/Invoices'
+// import InvoicePreview from './components/InvoicePreview'
+// import BusinessProfile from './pages/BusinessProfile'
+// import Notfound from './pages/Notfound'
 
+const ClerkProtected = ({ children }) => (
+  <>
+    <SignedIn>{children}</SignedIn>
+    <SignedOut>
+      <RedirectToSignIn />
+    </SignedOut>
+  </>
+)
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-    </Routes>
+    <div className='min-h-screen max-w-full overflow-x-hidden'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* it must be a protected route */}
+        <Route
+          path='/app'
+          element={
+            <ClerkProtected>
+              <AppShell />
+            </ClerkProtected>
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path='dashboard' element={<Dashboard />} />
+          {/* <Route path='invoices' element={<InvoicesPage />} />
+          <Route path='invoices/new' element={<CreateInvoice />} />
+          <Route path='invoices/:id' element={<InvoicePreview />} />
+          <Route path='invoices/:id/preview' element={<InvoicePreview />} />
+          <Route path='invoices/:id/edit' element={<CreateInvoice />} />
+
+          <Route path='create-invoice' element={<CreateInvoice />} />
+          <Route path='business' element={<BusinessProfile />} /> */}
+        </Route>
+
+        {/* <Route path='*' element={<Notfound />} /> */}
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/components/AppShell.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppShell = () => {
+  return (
+    <div>AppShell</div>
+  )
+}
+
+export default AppShell
