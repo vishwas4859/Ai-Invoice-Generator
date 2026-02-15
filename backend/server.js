92e468098c773cda6102ee9cb70adfd15c4bf348
Vishwas Chaurasia <www.vishwaschaurasia@gmail.com>
2026-02-15 11:03:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 4000;
 
 // MIDDLEWARES
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174','https://ai-invoice-generator.vercel.app'],
-    credentials: true
-}))
+  origin: true,
+  credentials: true
+}));
 app.use(clerkMiddleware())
 // app.use(express.json({limit: "20mb"}))
 // app.use(express.urlencoded({ limit: "20mb", extended: true }));
